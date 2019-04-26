4e5e4784fcca50342713d57e90df8f59108b6992
Pietro Passarelli - News Labs <pietro.passarelli@bbc.co.uk>
2019-04-26 11:00:09 +0100

    Api class
    a sort of 'sdk' module to wrap around the API end points from the express server, to provide a single point of contact for the react app to call the backend

--- packages/client/src/Api/index.js
+++ packages/client/src/Api/index.js
@@ -1,62 +1,83 @@
 import { isBrowser, isElectron, whichJsEnv } from '../Util/which-js-env/index.js';
 import postData from './post-data.js';
 
+window.DB = () => {console.log('in react test ');};
 class Api {
   /**
    * Projects URL
    */
-  // Projects URL index
-  getProjectsUrl() {
-    return `${ process.env.REACT_APP_SERVER_URL }/api/projects`;
-  }
-
-  //  Projects URL show
-  // eslint-disable-next-line class-methods-use-this
-  getProjectUrl(id) {
-    return `${ process.env.REACT_APP_SERVER_URL }/api/projects/${ id }`;
-  }
   // Projects URL create
   // eslint-disable-next-line class-methods-use-this
   createProjectUrl() {
     return `${ process.env.REACT_APP_SERVER_URL }/api/projects`;
   }
-
+  // Projects URL index
+  getProjectsUrl() {
+    return `${ process.env.REACT_APP_SERVER_URL }/api/projects`;
+  }
+  //  Projects URL show
+  // eslint-disable-next-line class-methods-use-this
+  getProjectUrl(id) {
+    return `${ process.env.REACT_APP_SERVER_URL }/api/projects/${ id }`;
+  }
   // Projects URL update
-
+  // eslint-disable-next-line class-methods-use-this
+  updateProjectUrl(id) {
+    return `${ process.env.REACT_APP_SERVER_URL }/api/projects/${ id }/edit`;
+  }
   // Projects URL  delete
+  // eslint-disable-next-line class-methods-use-this
+  deleteProjectUrl(id) {
+    return `${ process.env.REACT_APP_SERVER_URL }/api/projects/${ id }`;
+  }
 
   /**
    * Transcripts URL
    */
   // eslint-disable-next-line class-methods-use-this
-  getTranscripts(projectId) {
+  // get transcripts index
+  getTranscriptsUrl(projectId) {
     return `${ process.env.REACT_APP_SERVER_URL }/api/projects/${ projectId }/transcripts`;
   }
 
   // get Transcript show
   // eslint-disable-next-line class-methods-use-this
-  getTranscript(projectId, transcriptId) {
+  getTranscriptUrl(projectId, transcriptId) {
     return `${ process.env.REACT_APP_SERVER_URL }/api/projects/${ projectId }/transcripts/${ transcriptId }`;
+  }
+
+  // delete Transcript
+  // eslint-disable-next-line class-methods-use-this
+  deleteTranscriptUrl(projectId, transcriptId) {
+    return `${ process.env.REACT_APP_SERVER_URL }/api/projects/${ projectId }/transcripts/${ transcriptId }`;
+  }
+
+  /**
+   * Annotations URL
+   */
+  getAnnotationsUrl(projectId, transcriptId) {
+    return `${ process.env.REACT_APP_SERVER_URL }/api/projects/${ projectId }/transcripts/${ transcriptId }/annotations`;
   }
 
   /**
    * Projects
    */
   getProjects() {
-    if (isBrowser()) {
-      return fetch(this.getProjectsUrl(), { mode: 'cors' })
-        .then(res => res.json())
-        .then((json) => {
-          return json.projects;
-        });
-    }
-    if (isElectron()) {
-      // TODO:
-      return new Promise((resolve, reject) => {
-        resolve(false);
-        reject(false);
+    // if (isBrowser()) {
+    return fetch(this.getProjectsUrl(), { mode: 'cors' })
+      .then(res => res.json())
+      .then((json) => {
+        return json.projects;
       });
-    }
+    // }
+    // TODO: to call electron, call the window.DB functions implemented in `/packages/electron/db/db.js`
+    // if (isElectron()) {
+    // TODO:
+    // return new Promise((resolve, reject) => {
+    //   resolve(false);
+    //   reject(false);
+    // });
+    // }
     // TODO: CEP + error handling
   }
 
@@ -80,9 +101,56 @@ class Api {
         return json.project;
       });
   }
+
+  updateProject(id) {
+    return fetch(this.getProjectUrl(id), { mode: 'cors', method: 'PUT' })
+      .then(res => res.json())
+      .then((json) => {
+        return json.project;
+      });
+  }
+
+  deleteProject(id) {
+    return fetch(this.getProjectUrl(id), { mode: 'cors', method: 'DELETE' })
+      .then(res => res.json())
+      .then((json) => {
+        return json;
+      });
+  }
   /**
    * Transcripts
    */
+
+  // index
+  getTranscripts(projectId) {
+    return fetch(this.getTranscriptsUrl(projectId), { mode: 'cors' })
+      .then(res => res.json())
+      .then((json) => {
+        return json;
+      });
+  }
+
+  //new
+
+  // show
+  getTranscript(projectId, transcriptId) {
+    return fetch(this.getTranscriptUrl(projectId, transcriptId), { mode: 'cors' })
+      .then(res => res.json())
+      .then((json) => {
+        return json;
+      });
+  }
+
+  /**
+  * Annotations
+  */
+  getAnnotations(projectId, transcriptId) {
+    return fetch(this.getAnnotationsUrl(projectId, transcriptId), { mode: 'cors' })
+      .then(res => res.json())
+      .then((json) => {
+        return json;
+      });
+  }
 }
 
 // https://www.sitepoint.com/javascript-design-patterns-singleton/
