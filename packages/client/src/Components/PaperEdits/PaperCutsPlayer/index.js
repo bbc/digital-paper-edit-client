1229600e701b9905b7c0b9704f5e7fca15e2ee21
melnyczuk <howard.melnyczuk@bbc.co.uk>
2019-05-01 19:22:31 +0100

    Initial component outline

---
+++ packages/client/src/Components/PaperEdits/PaperCutsPlayer/index.js
@@ -0,0 +1,57 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+import VideoContext from 'videocontext';
+
+const getCanvas = (width, height) => {
+  return (
+    <canvas
+      className="video-canvas"
+      width={ width }
+      height={ height }
+    >
+    </canvas>
+  );
+};
+
+class PaperCutsPlayer extends React.PureComponent {
+  constructor(props) {
+    const {
+      width = 1280,
+      height = 720,
+      playlist = []
+    } = props;
+
+    super(props);
+    this.playlist = playlist;
+    this.canvas = getCanvas(width, height);
+    this.videoCtx = new VideoContext(this.canvas);
+  }
+
+  connect() {
+    // Connect the created track to the ouput canvas so it can be rendered.
+    // You could also connect the video track to an effect node if you wanted to do effects.
+    VideoContext
+      .importSimpleEDL(this.videoCtx, this.playlist)
+      .connect(this.videoCtx.destination);
+  }
+
+  play() {
+    this.videoCtx.play();
+  }
+
+  pause() {
+    this.videoCtx.pause();
+  }
+
+  render() {
+    return (
+      <>
+        {this.canvas}
+        {this.progressBar && this.progressBar}
+        {this.controls && this.controls}
+      </>
+    );
+  }
+}
+
+export default PaperCutsPlayer;
