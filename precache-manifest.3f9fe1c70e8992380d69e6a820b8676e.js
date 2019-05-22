5a880269fe564c060c7046a4609ce829e51ed12d
Pietro Passarelli - News Labs <pietro.passarelli@bbc.co.uk>
2019-05-22 15:26:57 +0100

    Updates

---
+++ precache-manifest.3f9fe1c70e8992380d69e6a820b8676e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "658ac9e377b9bde30bb2aec15fa29151",
+    "url": "./static/media/labsLogo.658ac9e3.png"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.8c97409f.js"
+  },
+  {
+    "revision": "6e585288fc9c6b82c765",
+    "url": "./static/js/main.6e585288.chunk.js"
+  },
+  {
+    "revision": "8feb5a3709a322b4e638",
+    "url": "./static/js/2.8feb5a37.chunk.js"
+  },
+  {
+    "revision": "6e585288fc9c6b82c765",
+    "url": "./static/css/main.048af529.chunk.css"
+  },
+  {
+    "revision": "8feb5a3709a322b4e638",
+    "url": "./static/css/2.f09b0ddd.chunk.css"
+  },
+  {
+    "revision": "42316347d8b97bf9cfcbcf4efbc47d97",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.e20e51a284eacaf63decdcd5676488c5.js"
+  "./precache-manifest.3f9fe1c70e8992380d69e6a820b8676e.js"
 );
 
 workbox.clientsClaim();
