df60d45bf6936fbbe22eff400141971ea15db5eb
Pietro Passarelli - News Labs <pietro.passarelli@bbc.co.uk>
2019-05-28 09:02:08 +0100

    refactor API Wrapper

--- src/ApiWrapper/index.js
+++ src/ApiWrapper/index.js
@@ -20,6 +20,12 @@ class ApiWrapper {
 
   annotationsUrl = (projectId, transcriptId) => `${ this.transcriptsIdUrl(projectId, transcriptId) }/annotations`;
 
+  annotationsIdUrl = (projectId, transcriptId, annotationId) => `${ this.transcriptsIdUrl(projectId, transcriptId) }/annotations/${ annotationId }`;
+
+  labelsUrl = (projectId) => `${ this.projectsIdUrl(projectId) }/labels`;
+
+  labelsIdUrl = (projectId, labelsId) => `${ this.projectsIdUrl(projectId) }/labels/${ labelsId }`;
+
   /**
    * Projects
    */
@@ -101,13 +107,81 @@ class ApiWrapper {
   /**
    * Annotations
    */
-  async getAnnotations(projectId, transcriptId) {
+  async getAllAnnotations(projectId, transcriptId) {
     const res = await fetch(this.annotationsUrl(projectId, transcriptId));
     const json = await res.json();
 
     return json;
   }
 
+  // not used
+  async getAnnotation(projectId, transcriptId, annotationId) {
+    const res = await corsFetch(this.annotationsIdUrl(projectId, transcriptId, annotationId));
+    const json = await res.json();
+
+    return json;
+  }
+
+  async createAnnotation(projectId, transcriptId, data) {
+    const res = await corsFetch(this.annotationsUrl(projectId, transcriptId), 'POST', data, 'json');
+
+    return await res.json();
+  }
+
+  async updateAnnotation(projectId, transcriptId, annotationId, data) {
+    const res = await corsFetch(this.annotationsIdUrl(projectId, transcriptId, annotationId), 'PUT', data);
+    const json = await res.json();
+
+    return json;
+  }
+
+  async deleteAnnotation(projectId, transcriptId, annotationId) {
+    const res = await corsFetch(this.annotationsIdUrl(projectId, transcriptId, annotationId), 'DELETE');
+    const json = await res.json();
+
+    return json;
+  }
+
+  /**
+   * Labels
+   */
+
+  // Get All Labels
+  async getAllLabels(projectId) {
+    const res = await fetch(this.labelsUrl(projectId));
+    const json = await res.json();
+
+    return json;
+  }
+  // Get Label - not used
+  async getLabel(projectId, labelId) {
+    const res = await corsFetch(this.labelsIdUrl(projectId, labelId));
+    const json = await res.json();
+
+    return json;
+  }
+
+  // Create Label
+  async createLabel(projectId, data) {
+    const res = await corsFetch(this.labelsUrl(projectId), 'POST', data, 'json');
+    const json = await res.json();
+
+    return json;
+  }
+  // Update Label
+  async updateLabel(projectId, labelId, data) {
+    const res = await corsFetch(this.labelsIdUrl(projectId, labelId), 'PUT', data);
+    const json = await res.json();
+
+    return json;
+  }
+  // Delete Label
+  async deleteLabel(projectId, labelId) {
+    const res = await corsFetch(this.labelsIdUrl(projectId, labelId), 'DELETE');
+    const json = await res.json();
+
+    return json;
+  }
   /**
    * PaperEdits
    */
@@ -125,7 +199,7 @@ class ApiWrapper {
     const res = await corsFetch(this.paperEditsIdUrl(projectId, id));
     const json = await res.json();
 
-    return json.paperedit;
+    return json;
   }
 
   async createPaperEdit(projectId, data) {
@@ -146,6 +220,32 @@ class ApiWrapper {
     const json = await res.json();
 
     return json;
+  }
+
+  /**
+   * Helper SDK function to avoid making multiple calls client side when in Annotated Transcript view
+   * Transcript + Annotations for that transcript + Labels for the project
+   */
+  async get_TranscriptLabelsAnnotations(projectId, transcriptId) {
+    // GET Transcripts
+    const transcriptResult = await this.getTranscript(projectId, transcriptId);
+    // GET Labels for Project <-- or separate request in label component
+    const labelsResults = await this.getAllLabels(projectId, transcriptId);
+    // GET Annotation for Transcript
+    const annotationsResult = await this.getAllAnnotations(projectId, transcriptId);
+
+    // Combine results
+    const results = {
+      // TODO: add a call to project to get title
+      projectTitle: transcriptResult.projectTitle,
+      transcriptTitle: transcriptResult.transcriptTitle,
+      url: transcriptResult.url,
+      labels: labelsResults.labels,
+      transcript:  transcriptResult.transcript,
+      annotations: annotationsResult.annotations
+    };
+
+    return results;
   }
 
 }
