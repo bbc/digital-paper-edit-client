2548eacd640fe9390f5161b064c8e3ca595ba774
James Dooley <james.dooley@bbc.co.uk>
2019-06-20 11:04:14 +0100

    Task: remove post-build step

--- bake-scripts/environment-config.py
+++ bake-scripts/environment-config.py
@@ -7,7 +7,6 @@ def main(argv):
     config_json = json.load(open(argv[1], "r"))
     api_url = config_json["configuration"]["API_URL"]
 
-    # with open('../public/env.js', 'w') as react_config:
     with open('/usr/lib/digital-paper-edit-client/build/env.js', 'w') as react_config:
         react_config.write("window.env = {{ API_URL: \"{0}\" }}".format(api_url));
 
