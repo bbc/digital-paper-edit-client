902609f2cd6dc529e686e4aa3666b84cbcb02225
Pietro Passarelli - News Labs <pietro.passarelli@bbc.co.uk>
2019-07-13 12:55:28 +0100

    if not programme script in list don't show search option

--- src/Components/lib/ListPage/index.js
+++ src/Components/lib/ListPage/index.js
@@ -40,14 +40,20 @@ class Page extends React.Component {
 
   render() {
 
+    let searchEl;
+    if (this.props.items !== null && this.props.items.length !== 0) {
+      searchEl = (<SearchBar
+        handleSearch={ this.handleSearch }
+      />);
+    }
+
     return (<>
 
       <Row>
         <Col sm={ 9 } md={ 9 } ld={ 9 } xl={ 9 }>
-          <SearchBar
-            handleSearch={ this.handleSearch }
-          />
+          {searchEl}
         </Col>
+
         <Col xs={ 12 } sm={ 3 } md={ 3 } ld={ 3 } xl={ 3 }>
           <Button onClick={ this.props.handleShowCreateNewItemForm } variant="outline-secondary" size="sm" block>
                 New {this.props.model}
