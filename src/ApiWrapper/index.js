351aa1f5c3e58e6eb5c2301d84a21a384a954273
Pietro <pietro.passarelli@bbc.co.uk>
2019-07-19 13:53:35 +0100

    Demo app for github pages  (#40)
    * final fix + clean up unecessary comments
    
    * Removed dbWrapper in DemoApiWrapper as not in use
    
    * Removed index demo logic
    
    as demo env is detected in ApiWrapper class
    
    * Removed unecessary console.log
    
    * moved demo check in whichJsEnv module
    
    * added prepublish step
    
    to prep build folder, and remove the db folder with json for client publishing

--- src/ApiWrapper/index.js
+++ src/ApiWrapper/index.js
@@ -1,15 +1,21 @@
 import ApiWrapper from './ApiWrapper';
-// import ElectronWrapper from './ElectronWrapper.js';
+import DemoApiWrapper from './DemoApiWrapper/index.js';
 import whichJsEnv from '../Util/which-js-env';
 
 const jsENV = whichJsEnv();
-// export default apiWrapper;
 
 // dynamic export
 // https://medium.com/@WebReflection/javascript-dynamic-import-export-b0e8775a59d4
 export default ( () => {
   // await async dependencies
   // export the module
+  if (jsENV === 'demo') {
+    console.log('API Wrapper demo time!');
+    const demoApiWrapper = new DemoApiWrapper();
+    Object.freeze(demoApiWrapper);
+
+    return demoApiWrapper;
+  }
   if (jsENV === 'browser') {
     const apiWrapper = new ApiWrapper();
     Object.freeze(apiWrapper);
