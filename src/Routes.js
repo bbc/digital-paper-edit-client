b52a486c68cc3622111beaeef506f1f658106113
Eimi Okuno <Eimi.Okuno@bbc.co.uk>
2019-10-19 13:53:09 +0100

    Adding debug router

--- src/Routes.js
+++ src/Routes.js
@@ -5,15 +5,28 @@ import TranscriptEditor from './Components/Workspace/Transcripts/TranscriptEdito
 import PaperEditor from './Components/PaperEditor';
 import { Switch, Route, Redirect, BrowserRouter } from 'react-router-dom';
 
+class DebugRouter extends BrowserRouter {
+  constructor(props) {
+    super(props);
+    console.log('initial history is: ', JSON.stringify(this.history, null, 2));
+    this.history.listen((location, action) => {
+      console.log(
+        `The current URL is ${ location.pathname }${ location.search }${ location.hash }`
+      );
+      console.log(`The last navigation action was ${ action }`, JSON.stringify(this.history, null, 2));
+    });
+  }
+}
+
 const NoMatch = () => {
   return <h1>There was an error loading the page you requested</h1>;
 };
 
 const Routes = () => {
   return (
-    <BrowserRouter>
+    <DebugRouter>
       <Switch>
-        <Redirect exact from="/" to="/projects" />
+        <Redirect from="/" to="/projects" />
         <Route exact path="/projects" component={ Projects } />
         <Route exact path="/projects/:projectId" component={ Workspace } />
         <Route
@@ -28,7 +41,7 @@ const Routes = () => {
         />
         <Route component={ NoMatch } />
       </Switch>
-    </BrowserRouter>
+    </DebugRouter>
   );
 };
 
