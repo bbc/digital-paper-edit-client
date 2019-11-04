cde4729f6e2a7b06e576e9d82f9bac813b6b2aaa
Eimi Okuno <eimi.okuno@bbc.co.uk>
2019-11-04 20:54:27 +0000

    Proper JSEnv not picked up based on logic order

--- src/Util/which-js-env/index.js
+++ src/Util/which-js-env/index.js
@@ -3,46 +3,41 @@
  * @returns - 'browser', 'electron', 'cep'. where cep stands for adobe CEP panel
  */
 function whichJsEnv() {
-// Is browser Eg client side app or locally testing (Node)
-  // if (window.process === undefined) {
-  if (window.process === undefined || window.process.versions.node !== undefined) {
-    // console.debug('In browser Environment');
+  if (window.process.versions.electron !== undefined) {
+    return 'electron';
+  }
+  // Is browser Eg client side app or locally testing (Node)
+  if (
+    window.process === undefined ||
+    window.process.versions.node !== undefined
+  ) {
     if (process.env.REACT_APP_NODE_ENV === 'demo') {
       return 'demo';
     }
 
     return 'browser';
-  } else {
-    if (window.process.versions.electron !== undefined) {
-      //   console.debug('In Electron Enviroment');
-      //   console.info('In Electron v: ', window.process.versions.electron);
-      //   console.info('Using chrome v: ', window.process.versions.chrome);
-      return 'electron';
-    }
-    else if (window.process.versions.cep !== undefined) {
-      //   console.debug('In Adobe CEP Environment');
-      //   console.info('In Chromium v: ', window.process.versions.chromium);
-      //   console.info('In CEP (Adobe Common Extensibility Platform ) v: ', window.process.versions.cep);
-      //   console.info('adjusting current working directory for Adobe CEP');
-      //   console.log('Starting directory: ' + process.cwd());
-      try {
-        process.chdir(__dirname);
-        // process.chdir('..');
-        // console.log('New directory: ' + process.cwd());
-      }
-      catch (err) {
-        console.log('chdir: ' + err);
-      }
-
-      return 'cep';
-
-    }
-    else {
-      console.error("couldn't determine the js environment");
-
-      return undefined;
-    }
   }
+
+  if (window.process.versions.cep !== undefined) {
+    //   console.debug('In Adobe CEP Environment');
+    //   console.info('In Chromium v: ', window.process.versions.chromium);
+    //   console.info('In CEP (Adobe Common Extensibility Platform ) v: ', window.process.versions.cep);
+    //   console.info('adjusting current working directory for Adobe CEP');
+    //   console.log('Starting directory: ' + process.cwd());
+    try {
+      process.chdir(__dirname);
+      // process.chdir('..');
+      // console.log('New directory: ' + process.cwd());
+    } catch (err) {
+      console.log('chdir: ' + err);
+    }
+
+    return 'cep';
+  }
+
+  console.error("couldn't determine the js environment");
+
+  return undefined;
 }
 
 function isBrowser() {
