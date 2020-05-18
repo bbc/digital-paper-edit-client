af692d2e10e2f6276c7042a30595aa95f09c496b
Pietro Passarelli <pietro.passarelli@wsj.com>
2020-05-18 13:45:17 -0400

    1.8.2
    trying to avoid to keep the whole electron wrapper in browser window

--- src/ApiWrapper/index.js
+++ src/ApiWrapper/index.js
@@ -23,26 +23,44 @@ export default (() => {
     return apiWrapper;
   }
   if (jsENV === 'electron') {
-    try {
-      const { ipcRenderer } = require('electron');
-      const appPath = ipcRenderer.sendSync('synchronous-message-get-app-path', 'ping');
-      const path = require('path');
-      window.process.chdir(appPath);
-      const ElectronWrapper = require(path.join(appPath, 'src', 'ElectronWrapper', 'index.js'));
-      // const ElectronWrapper = window.ElectronWrapper;
-      const electronWrapper = new ElectronWrapper();
-      Object.freeze(electronWrapper);
+    // try {
+    //   const { ipcRenderer } = require('electron');
+    //   const appPath = ipcRenderer.sendSync('synchronous-message-get-app-path', 'ping');
+    const path = require('path');
+    //   window.process.chdir(appPath);
+    const ElectronWrapper = require(path.join(window.appPath, 'src', 'ElectronWrapper', 'index.js'));
+    // const ElectronWrapper = window.ElectronWrapper;
+    const electronWrapper = new ElectronWrapper();
+    Object.freeze(electronWrapper);
 
-      return electronWrapper;
-    } catch (e) {
-      console.error('api wrapper electron', e);
-    }
+    return electronWrapper;
+    // } catch (e) {
+    //   console.error('api wrapper electron', e);
+    // }
   }
   if (jsENV === 'cep') {
+    // TODO: not sure if this will work
+    // try {
+    // console.log('__dirname', __dirname);
+    // console.log('process.cwd()', process.cwd());
+    // /////////////////////////
+    // console.log('  process.chdir(__dirname);');
+    // process.chdir(__dirname);
+    // /////////////////////////
+    // console.log('__dirname', __dirname);
+    // console.log('process.cwd()', process.cwd());
+
+    // const path = require('path');
+    // const AdobeCEPWrapper = require(path.join(__dirname, 'src', 'AdobeCEPWrapper', 'index.js'));
+    window.AdobeCEPWrapper = AdobeCEPWrapper;
+
     const AdobeCEPWrapper = window.AdobeCEPWrapper;
     const adobeCEPWrapper = new AdobeCEPWrapper();
     Object.freeze(adobeCEPWrapper);
 
     return adobeCEPWrapper;
+    // } catch (err) {
+    //   console.log('chdir: ' + err);
+    // }
   }
 })();
